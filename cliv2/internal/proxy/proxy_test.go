bd71546715a98cfdebc713d703ad2eb71d712e42
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2022-07-08 18:13:33 +0200

    chore: remove outdated tests
    Signed-off-by: Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>

--- cliv2/internal/proxy/proxy_test.go
+++ cliv2/internal/proxy/proxy_test.go
@@ -139,50 +139,6 @@ func Test_xSnykCliVersionHeaderIsReplaced(t *testing.T) {
 	wp.Close()
 }
 
-// func Test_GetProxyConnectHeader_successful01(t *testing.T) {
-// 	var err error
-// 	var objectUnderTest *proxy.WrapperProxy
-// 	var url *url.URL
-// 	var actualHeader http.Header
-
-// 	expectedHeader := make(http.Header)
-// 	expectedHeader.Add(httpauth.ProxyAuthorizationKey, "Mock")
-
-// 	debugLogger := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
-// 	objectUnderTest, err = proxy.NewWrapperProxy(false, "", "", debugLogger)
-// 	assert.Nil(t, err)
-
-// 	url, err = url.Parse("http://localhost:3128")
-// 	assert.Nil(t, err)
-
-// 	objectUnderTest.SetUpstreamProxyAuthentication(httpauth.Mock)
-
-// 	actualHeader, err = objectUnderTest.GetProxyConnectHeader(nil, url, "")
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, expectedHeader, actualHeader)
-// }
-
-// func Test_GetProxyConnectHeader_failed01(t *testing.T) {
-// 	var err error
-// 	var objectUnderTest *proxy.WrapperProxy
-// 	var url *url.URL
-// 	var actualHeader http.Header
-
-// 	expectedHeader := make(http.Header)
-
-// 	debugLogger := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
-// 	objectUnderTest, err = proxy.NewWrapperProxy(false, "", "", debugLogger)
-// 	assert.Nil(t, err)
-
-// 	url = nil // error case where url is nil
-
-// 	objectUnderTest.SetUpstreamProxyAuthentication(httpauth.Mock)
-
-// 	actualHeader, err = objectUnderTest.GetProxyConnectHeader(nil, url, "")
-// 	assert.Equal(t, expectedHeader, actualHeader)
-// 	assert.NotNil(t, err)
-// }
-
 func Test_SetUpstreamProxy(t *testing.T) {
 
 	var err error
